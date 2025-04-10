e565fd39a111cced05b297fe4f6b623c47fab9ec
smGarc <sgarcia4@stevens.edu>
2025-04-10 15:44:33 -0400

    Add functionality for two sides of motors

--- motors.py
+++ motors.py
@@ -1,70 +1,99 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-IN1 = 24
-IN2 = 23
-EN = 25
-TEMP1 = 1
+RIN1 = 24
+RIN2 = 23
+REN = 25
+LIN1 = 17
+LIN2 = 4
+LEN = 27
+DIR = 1
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(EN, GPIO.OUT)
-GPIO.output(IN1, GPIO.LOW)
-GPIO.output(IN2, GPIO.LOW)
-p = GPIO.PWM(EN, 1000)
+GPIO.setup(RIN1, GPIO.OUT)
+GPIO.setup(RIN2, GPIO.OUT)
+GPIO.setup(REN, GPIO.OUT)
+GPIO.output(RIN1, GPIO.LOW)
+GPIO.output(RIN2, GPIO.LOW)
+p = GPIO.PWM(REN, 1000)
 p.start(25)
 print("\n")
-print("Default speed and direction of motor is LOW and Forward...")
-print("R-run, S-stop, F-forward, B-backwards, L-low, M-medium, H-high, E-exit")
+print("Default speed and direction of motor is Slow and Forward...")
+print("D-Drive, P-Park, F-Forward, B-Backwards, R-Turn Right, L-Turn Left,\nS-Slow, M-Medium, H-High, E-Exit")
 print("\n")
 
 while(1):
-	x = input()
-	if x == 'r':
-		print("Run")
-		if TEMP1 == 1:
-			GPIO.output(IN1, GPIO.HIGH)
-			GPIO.output(IN2, GPIO.LOW)
-			print("Forward")
-			x = 'z'
-		else:
-			GPIO.output(IN1, GPIO.LOW)
-			GPIO.output(IN2, GPIO.HIGH)
-			print("Backward")
-			x = 'z'
-	elif x == 's':
-		print("Stop")
-		GPIO.output(IN1, GPIO.LOW)
-		GPIO.output(IN2, GPIO.LOW)
-		x = 'z'
-	elif x == 'f':
-		print("Forward")
-		GPIO.output(IN1, GPIO.HIGH)
-		GPIO.output(IN2, GPIO.LOW)
-		TEMP1 = 1
-		x = 'z'
-	elif x == 'b':
-		print("Backwards")
-		GPIO.output(IN1, GPIO.LOW)
-		GPIO.output(IN2, GPIO.HIGH)
-		TEMP1 = 0
-		x = 'z'
-	elif x == 'l':
-		print("Low")
-		p.ChangeDutyCycle(25)
-		x = 'z'
-	elif x == 'm':
-		print("Medium")
-		p.ChangeDutyCycle(50)
-		x = 'z'
-	elif x == 'h':
-		print("High")
-		p.ChangeDutyCycle(75)
-		x = 'z'
-	elif x == 'e':
-		GPIO.cleanup()
-		break
-	else:
-		print("Wrong command")
-		print("Please enter a correct command to continue")
+  x = input()
+  if x == 'd':
+    print("Drive")
+    if DIR == 1:
+      GPIO.output(RIN1, GPIO.HIGH)
+      GPIO.output(RIN2, GPIO.LOW)
+      GPIO.output(LIN1, GPIO.HIGH)
+      GPIO.output(LIN2, GPIO.LOW)
+      print("Forward")
+      x = 'z'
+    else:
+      GPIO.output(RIN1, GPIO.LOW)
+      GPIO.output(RIN2, GPIO.HIGH)
+      GPIO.output(LIN1, GPIO.LOW)
+      GPIO.output(LIN2, GPIO.HIGH)
+      print("Backward")
+      x = 'z'
+
+  elif x == 'p':
+    print("Park")
+    GPIO.output(RIN1, GPIO.LOW)
+    GPIO.output(RIN2, GPIO.LOW)
+    x = 'z'
+
+  elif x == 'f':
+    print("Forward")
+    GPIO.output(RIN1, GPIO.HIGH)
+    GPIO.output(RIN2, GPIO.LOW)
+    DIR = 1
+    x = 'z'
+
+  elif x == 'b':
+    print("Backwards")
+    GPIO.output(RIN1, GPIO.LOW)
+    GPIO.output(RIN2, GPIO.HIGH)
+    DIR = 0
+    x = 'z'
+
+  elif x == 'r':
+    print("Turn Right")
+    GPIO.output(RIN1, GPIO.LOW)
+    GPIO.output(RIN2, GPIO.HIGH)
+    GPIO.output(LIN1, GPIO.LOW)
+    GPIO.output(LIN2, GPIO.LOW)
+
+  elif x == 'l':
+    print("Turn Left")
+    GPIO.output(RIN1, GPIO.LOW)
+    GPIO.output(RIN2, GPIO.LOW)
+    GPIO.output(LIN1, GPIO.HIGH)
+    GPIO.output(LIN2, GPIO.HIGH)
+
+  elif x == 's':
+    print("Low")
+    p.ChangeDutyCycle(25)
+    x = 'z'
+
+  elif x == 'm':
+    print("Medium")
+    p.ChangeDutyCycle(50)
+    x = 'z'
+
+  elif x == 'h':
+    print("High")
+    p.ChangeDutyCycle(75)
+    x = 'z'
+
+  elif x == 'e':
+    GPIO.cleanup()
+    break
+
+  else:
+    print("Wrong command")
+    print("Please enter a correct command to continue")
