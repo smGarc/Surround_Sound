afc39bde5eeb91e782d1fa991eb628254f8352d0
smGarc <sgarcia4@stevens.edu>
2025-04-28 13:41:03 -0400

    Updated motors.drive()

--- motors.py
+++ motors.py
@@ -9,24 +9,6 @@ LIN2 = 4
 EN = 25
 DIR = 1
 
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RIN1, GPIO.OUT)
-GPIO.setup(RIN2, GPIO.OUT)
-GPIO.setup(LIN1, GPIO.OUT)
-GPIO.setup(LIN2, GPIO.OUT)
-GPIO.setup(EN, GPIO.OUT)
-GPIO.output(RIN1, GPIO.LOW)
-GPIO.output(RIN2, GPIO.LOW)
-GPIO.output(LIN1, GPIO.LOW)
-GPIO.output(LIN2, GPIO.LOW)
-p = GPIO.PWM(EN, 1000)
-p.start(25)
-print("\n")
-print("Default speed and direction of motor is Slow and Forward...")
-print("W-Forward, A-Left, S-Backward, D-Left\nZ-Slow, X-Medium, C-High, E-Exit")
-print("\n")
-
 def getch():
   fd = sys.stdin.fileno()
   old_settings = termios.tcgetattr(fd)
@@ -45,9 +27,26 @@ def resetMotors():
   GPIO.output(LIN2, GPIO.LOW)
 
 def drive():
+  GPIO.cleanup()
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(RIN1, GPIO.OUT)
+  GPIO.setup(RIN2, GPIO.OUT)
+  GPIO.setup(LIN1, GPIO.OUT)
+  GPIO.setup(LIN2, GPIO.OUT)
+  GPIO.setup(EN, GPIO.OUT)
+  GPIO.output(RIN1, GPIO.LOW)
+  GPIO.output(RIN2, GPIO.LOW)
+  GPIO.output(LIN1, GPIO.LOW)
+  GPIO.output(LIN2, GPIO.LOW)
+  p = GPIO.PWM(EN, 1000)
+  p.start(25)
+  print("\n")
+  print("Default speed and direction of motor is Slow and Forward...")
+  print("W-Forward, A-Left, S-Backward, D-Left\nZ-Slow, X-Medium, C-High, E-Exit")
+  print("\n")
   while(1):
     x = getch()
-    
+
     if x == 'w':
       print("Forward")
       GPIO.output(RIN1, GPIO.HIGH)
